144a2311d8f39d45a86d45947c3f8b45df7046dd
felixtitonina <felixtitonina@outlook.com>
2020-10-22 01:20:01 -0300

    Inserindo multer

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -10,11 +10,22 @@ export default {
         return res.status(200).json(orphanages)
     },
     async show(req: Request, res: Response){
-        const { id } = req.params
-        const orphanagesRepository = getRepository(Orphanage);
-        const orphanages = await orphanagesRepository.findOneOrFail(id)
+        try {
+            const { id } = req.params
+            const orphanagesRepository = getRepository(Orphanage);
+            const orphanages = await orphanagesRepository.findOneOrFail(id)
+            console.log(orphanages);
+            
+            return res.status(200).json(orphanages)
 
-        return res.status(200).json(orphanages)
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                error: error.message,
+                msg: 'Não foi possível localizar o id informado'
+            })
+        }
+
     },
     async create(req: Request, res: Response){
         const {
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,24 @@
 import {Router} from 'express'
+import multer from 'multer'
+
+import uploadConfig from './config/upload'
 import OrphanageController from './controllers/OrphanagesController'
+
 const routes = Router()
+const upload = multer(uploadConfig)
+
 routes.get('/orphanages', OrphanageController.index) 
 routes.get('/orphanages/:id', OrphanageController.show) 
-routes.post('/orphanages', OrphanageController.create) 
+routes.post('/orphanages', upload.array('images') ,OrphanageController.create) 
 
 export default routes;
+
+    // "name": "Felix tito nina",
+    // "latitude": -23.4543142,
+    // "longitude": -46.3912693,
+    // "about": "Sobre orfa",
+    // "instructions": "venha visitar",
+    // "opening_hours": "das 8 as 18 ",
+    // "open_on_weekends": true
+
+    // parada 01:22
